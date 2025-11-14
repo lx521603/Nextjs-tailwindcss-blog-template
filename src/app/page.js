2bf9ca28e90bb1536cf4efd7abd76260b153b6fc
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-14 20:34:10 +0800

    更新 page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,16 @@
-import {blogs} from "@/.velite/generated";
+import { blogs } from "@/.velite/generated";
 import HomeCoverSection from "../components/Home/HomeCoverSection";
 import FeaturedPosts from "../components/Home/FeaturedPosts";
 import RecentPosts from "../components/Home/RecentPosts";
 
 export default function Home() {
+  const safeBlogs = Array.isArray(blogs) ? blogs : [];
+
   return (
     <main className="flex flex-col items-center justify-center">
-      <HomeCoverSection blogs={blogs} />
-      <FeaturedPosts blogs={blogs} />
-      <RecentPosts blogs={blogs} />
+      <HomeCoverSection blogs={safeBlogs} />
+      <FeaturedPosts blogs={safeBlogs} />
+      <RecentPosts blogs={safeBlogs} />
     </main>
-  )
+  );
 }

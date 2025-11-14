07dd3717a43d8ac8c8ff30ebe0b1a658079b8cea
lx521603 <32638642+lx521603@users.noreply.github.com>
2025-11-14 21:46:06 +0800

    更新 siteMetaData.js

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,22 +1,22 @@
 // please update required information
 const siteMetadata = {
-    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
-    author: 'CodeBucks',
-    headerTitle: 'Next.js Blog',
-    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
-    language: 'en-us',
+    title: '童心未泯',
+    author: 'Sai',
+    headerTitle: '童心未泯',
+    description: '这里是描述',
+    language: 'zj-CN',
     theme: 'system', // system, dark or light
     siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
     siteLogo: '/logo.png',
     socialBanner: '/social-banner.png', // add social banner in the public folder
-    email: 'codebucks27@gmail.com', 
+    email: 'x@sai.st', 
     github: 'https://github.com/codebucks27',
     twitter: 'https://twitter.com/code_bucks',
     facebook: 'https://facebook.com',
     youtube: 'https://youtube.com/codebucks',
     linkedin: 'https://www.linkedin.com/in/codebucks/',
     dribbble: 'https://www.dribbble.com',
-    locale: 'en-US',
+    locale: 'zh-CN',
   }
   
   module.exports = siteMetadata
